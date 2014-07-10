57e97a7aff158978ca91e67285e619f73d96714b
Nicolas Hainaux <nh.techn@gmail.com>
2014-07-10 17:40:35 +0200

    Added the licence, eliminated import errors, first test

--- lib/states/State.py
+++ lib/states/State.py
@@ -1,25 +1,35 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Pilberry runs a car radio based on Raspberry Pi
+# Copyright 2014 Olivier Cecillon <ocecillon@users.sourceforge.net>
+# and Nicolas Hainaux <nico_h@users.sourceforge.net>
+
+# This file is part of Pilberry.
+
+# Pilberry is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Pilberry is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Pilberry; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 from abc import ABCMeta, abstractmethod
-from base import Clonable
+from ..base import Clonable
 
-HANDLE = {'CMD_DIR_UP' : handle_cmd_dir_up,
-          'CMD_DIR_DOWN' : handle_cmd_dir_down,
-          'CMD_PLAY' : handle_cmd_play,
-          'CMD_PREV' : handle_cmd_prev,
-          'CMD_NEXT' : handle_cmd_next,
-          'CMD_ESC' : handle_cmd_esc,
-          'CMD_STOP' : handle_cmd_stop,
-          'CMD_CHMOD' : handle_cmd_chmod
-         }
+
 
 class State(Clonable, metaclass=ABCMeta):
 
     # def handle(self, cmd):
     #    self.locals()['handle_cmd_' + cmd]()
-
-    def handle(self, cmd):
-        HANDLE[cmd]()
 
     @abstractmethod
     def handle_cmd_dir_up(self):
@@ -53,4 +63,15 @@ class State(Clonable, metaclass=ABCMeta):
     def handle_cmd_chmod(self):
         pass
 
+    HANDLE = {'CMD_DIR_UP' : handle_cmd_dir_up,
+              'CMD_DIR_DOWN' : handle_cmd_dir_down,
+              'CMD_PLAY' : handle_cmd_play,
+              'CMD_PREV' : handle_cmd_prev,
+              'CMD_NEXT' : handle_cmd_next,
+              'CMD_ESC' : handle_cmd_esc,
+              'CMD_STOP' : handle_cmd_stop,
+              'CMD_CHMOD' : handle_cmd_chmod
+             }
 
+    def handle(self, cmd):
+        HANDLE[cmd]()
--- lib/states/State_A.py
+++ lib/states/State_A.py
@@ -1,10 +1,35 @@
 # -*- coding: utf-8 -*-
 
-from . import State
+# Pilberry runs a car radio based on Raspberry Pi
+# Copyright 2014 Olivier Cecillon <ocecillon@users.sourceforge.net>
+# and Nicolas Hainaux <nico_h@users.sourceforge.net>
+
+# This file is part of Pilberry.
+
+# Pilberry is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Pilberry is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Pilberry; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+from .State import State
 
 global current_state
 
-class State_A(State.State):
+class State_A(State):
+
+    def __init__(self):
+        pass
+
     def handle_cmd_dir_up(self):
         print("handle_cmd_dir_up from State_A")
 
