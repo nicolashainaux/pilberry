8d5e9ab8caff20c09e38853b9d5f372db458f30f
Nicolas Hainaux <nh.techn@gmail.com>
2014-07-17 10:00:26 +0200

    Some changes, mainly in documentation and the nature of current_state

--- lib/globals.py
+++ lib/globals.py
@@ -45,6 +45,7 @@ for s in MODES_CONFIG.sections():
 ##
 #   @todo   At startup, define the current_mode as last used mode
 current_mode = modes_list[0]
+current_state = {}
 
 # Read the modes' list from appropriate conf file
 CMD_CONFIG = configparser.ConfigParser()
