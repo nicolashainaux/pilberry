7fa468ee8c5d1453344c10daff476b4b8937f745
Nicolas Hainaux <nh.techn@gmail.com>
2014-07-31 11:58:34 +0200

    Raw structure of a MusicDriver class

---
+++ lib/musicdriver/MusicDriver.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Pilberry runs a car radio based on Raspberry Pi
+# Copyright 2014 Olivier Cecillon <ocecillon@users.sourceforge.net>
+# and Nicolas Hainaux <nico_h@users.sourceforge.net>
+
+# This file is part of Pilberry.
+
+# Pilberry is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Pilberry is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Pilberry; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# Python packages|modules imports
+
+# Pilberry packages|modules imports
+
+##
+# @class MusicDriver
+# @brief The MusicDriver will drive the music player
+class MusicDriver(object):
+
+    ##
+    #   @brief
+    def __init__(self):
+        pass
+
+
+    ##
+    #   @brief To be sure to quit cmus safely, whatever happens. An __enter__()
+    #          and an __exit__() methods are required to be able to use the
+    #          object in a with statement.
+    def __enter__(self):
+        return self
+
+
+    ##
+    #   @brief To be sure to quit cmus safely, whatever happens. An __enter__()
+    #          and an __exit__() methods are required to be able to use the
+    #          object in a with statement.
+    def __exit__(self, type, value, traceback):
+        pass
+
