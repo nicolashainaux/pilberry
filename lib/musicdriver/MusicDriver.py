1bcd4689510d4dbfd74809c3055b291600d652d2
Nicolas Hainaux <nh.techn@gmail.com>
2014-08-19 18:32:46 +0200

    Removed unused import in MusicDriver.

--- lib/musicdriver/MusicDriver.py
+++ lib/musicdriver/MusicDriver.py
@@ -27,7 +27,7 @@ import logging
 
 # Pilberry packages|modules imports
 import lib.globals as globals
-from lib.globals import SOCKETS_CONFIG, AUDIO_FEEDBACK_LOCK_FILE
+from lib.globals import SOCKETS_CONFIG
 
 
 ##
