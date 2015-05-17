aa81f7deaa1ab280de26fa03d36d8d12aeecc2f2
Nicolas Hainaux <quamenzullo@users.noreply.github.com>
2015-05-17 11:29:01 +0200

    Finished renaming queue_first() into queue_song_first().

--- lib/mode/state/memory/default/State_B.py
+++ lib/mode/state/memory/default/State_B.py
@@ -85,7 +85,7 @@ class State_B(State):
     def move_to_node_prev(self):
         # We compute the new position with a modulo to go to first position if
         # we were at end and vice-versa
-        self.md.queue_first(self.head.full_path)
+        self.md.queue_song_first(self.head.full_path)
 
         stateBLog.debug("self.xnode.position - 1 = " \
                         + str(self.xnode.position - 1))
@@ -103,7 +103,7 @@ class State_B(State):
             if self.head.position == len(self.xnode.neighbours) - 1:
                 self.md.clear_queue()
 
-            self.md.queue_first(self.head.full_path)
+            self.md.queue_song_first(self.head.full_path)
             self.md.skip_to_next_song()
 
 
